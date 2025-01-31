1e14dd131343b5ef6547e23c4a702311117bc9d2
NadhipGitHub <muhammadnadiprahmatilah@gmail.com>
2025-01-31 09:12:00 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/', function () {
+   // return view('welcome');
+//});
+
+
+Route::get('/n1', function () {
+    return view('n1');
+});
+
+Route::get('/n', function () {
+    return view('n');
+});
